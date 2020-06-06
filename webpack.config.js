c5e7a1a0a7d9551992e71beda8c68ca01b9aae29
John Taylor <john.taylor@holidayextras.com>
2020-06-06 22:54:09 +0100

    Initial structure, dist output and babel loading

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    filename: 'boids.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  }
+}
