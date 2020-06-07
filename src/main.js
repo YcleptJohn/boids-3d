d8e2ed360bf619d49f64e9a78d8a3df9c349229c
John Taylor <john.taylor@holidayextras.com>
2020-06-07 16:12:49 +0100

    Demo: orbitControls, lights, gridhelper

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 
 let scene, cam, cube, renderer
 
@@ -11,13 +13,23 @@ function init() {
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
+  var controls = new OrbitControls(cam, renderer.domElement)
 
   var geometry = new THREE.BoxGeometry()
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  var material = new THREE.MeshPhongMaterial({ color: 0x44aa88 })
   cube = new THREE.Mesh( geometry, material )
   scene.add(cube)
 
   cam.position.z = 5;
+  controls.update()
+
+  var gridHelper = new THREE.GridHelper(10, 10, 0xFFFFFF, 0xFFFFFF)
+  scene.add(gridHelper)
+
+  var light = new THREE.DirectionalLight(0xFFFFFF, 1)
+  light.position.set(-1, 2, 4);
+  scene.add(light)
+
 }
 
 function anim() {
